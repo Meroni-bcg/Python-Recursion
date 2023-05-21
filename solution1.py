c748283a12358109e188c0cd00e97ade9d53c1b5
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-05-21 10:44:42 -0700

    solution1

--- solution1.py
+++ solution1.py
@@ -1,1 +1,29 @@
 # Write code for algorithm 1 below
+
+def count_down(n):
+
+  #  Base case
+  if n==0:
+      return
+
+  #  Recursive case
+  else:
+      print(n)
+      count_down(n-1)
+
+# test case
+n=8
+count_down(n)
+
+# Another option is
+def count_down(n):
+  #inherent base case
+  #(will stop if n <= 0)
+  if n>0:
+      #recursive case
+      print(n)
+      count_down(n-1)
+   
+#test case
+n=8
+count_down(n)
