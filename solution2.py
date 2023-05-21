cbf44224a1c345670ba6b56dc157af74da4d962c
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-05-21 10:46:12 -0700

    solution 2

--- solution2.py
+++ solution2.py
@@ -1,1 +1,10 @@
 # Write code for algorithm 2 below
+def natural_numbers(x,i):
+	#your code here
+    natural_numbers(3)
+#output: 1 2 3
+
+#What could be a base case for this problem?
+#If the number we are trying to print is greater than n, stop the function.
+
+
